dd7a4e60a871671c78af60ad9b034b25fd5679a9
Aluno <Aluno@LAB-1021>
2026-08-28 20:05:56 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using DW01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DW01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Professor()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Produto()
+        {
+            return View();
+        }
+
+        public IActionResult DetalhesProduto(int id)
+        {
+            return Content($"Produto recebido: {id}");
+        }
+
+        public IActionResult BuscarProduto(string? nome)
+        {
+            if (string.IsNullOrEmpty(nome))
+                return View();
+
+
+            return Content($"Produto pesquisado: {nome}");
+
+      
+
+        }
+      
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
