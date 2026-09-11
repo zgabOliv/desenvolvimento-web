4bdced37c208dfb68872a9ff63188262b4a86c64
Aluno <Aluno@LAB-1021>
2026-09-11 20:31:21 -0300

    Aula 11/09

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DW01.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DW01.Data
+
+{
+    public class ApplicationDbContext : DbContext
+    {
+           public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+            {
+            }
+            public DbSet<Produto> Produtos { get; set; }
+
+        }
+}
